c9e1dbe7aad86c9e27adcf98051bc9d578a37301
Victor Paul Choudhury <107463059+XKOBX@users.noreply.github.com>
2024-08-11 13:34:49 +0530

    resume updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const newtab=()=>{
-    window.open("https://drive.google.com/file/d/1OIUgK54ktoCixsowBlARgAhK2TmyYT0m/view?usp=sharing")
-  }
+  const newtab = () => {
+    window.open(
+      "https://drive.google.com/file/d/1OIUgK54ktoCixsowBlARgAhK2TmyYT0m/view?usp=sharing"
+    );
+  };
 
   const [icon, setIcon] = useState(false);
 
@@ -14,32 +16,35 @@ const Navbar = () => {
     {
       id: 1,
       link: "home",
-      class:"nav-link home",
+      class: "nav-link home",
     },
     {
       id: 2,
       link: "about",
-      class:"nav-link about",
+      class: "nav-link about",
     },
     {
       id: 3,
       link: "skills",
-      class:"nav-link skills",
+      class: "nav-link skills",
     },
     {
       id: 4,
       link: "projects",
-      class:"nav-link projects",
+      class: "nav-link projects",
     },
     {
       id: 5,
       link: "contact",
-      class:"nav-link contact",
+      class: "nav-link contact",
     },
   ];
 
   return (
-    <div id="nav-menu" className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4 z-40">
+    <div
+      id="nav-menu"
+      className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4 z-40"
+    >
       <div>
         <h1 className="text-3xl font-signature ml-2">vpc.</h1>
       </div>
@@ -52,17 +57,26 @@ const Navbar = () => {
           >
             <button className="cursor-pointer px-3 text-black hover:scale-105 duration-50  bg-white text-xl font-black uppercase items-center rounded-md">
               <Link to={link} smooth duration={300}>
-                  {link}
+                {link}
               </Link>
             </button>
           </li>
-
         ))}
-         <a class="nav-link resume" href="/fw20_0705-Victor-Choudhury-Resume.pdf" download={true}>
-        <button id="resume-button-1" onClick={()=>newtab()} className="cursor-pointer px-2 ml-1 text-black hover:scale-105 duration-50  bg-white text-xl font-black uppercase items-center rounded-md">RESUME</button>      
+        <a
+          class="nav-link resume"
+          href="/Victor-Choudhury--Resume.pdf"
+          download={true}
+        >
+          <button
+            id="resume-button-1"
+            onClick={() => newtab()}
+            className="cursor-pointer px-2 ml-1 text-black hover:scale-105 duration-50  bg-white text-xl font-black uppercase items-center rounded-md"
+          >
+            RESUME
+          </button>
         </a>
-        </ul>
-       
+      </ul>
+
       <div
         onClick={() => setIcon(!icon)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
@@ -87,9 +101,19 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
-            <a class="nav-link resume" href="/fw20_0705-Victor-Choudhury-Resume.pdf" download={true}>
-        <button id="resume-button-1" onClick={()=>newtab()} className="px-4 cursor-pointer capitalize py-6 text-4xl">Resume</button>      
-        </a>
+          <a
+            class="nav-link resume"
+            href="/fw20_0705-Victor-Choudhury-Resume.pdf"
+            download={true}
+          >
+            <button
+              id="resume-button-1"
+              onClick={() => newtab()}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              Resume
+            </button>
+          </a>
         </ul>
       )}
     </div>
